b446beb866dc9bf16475bc9bc693c21722f3444a
DuyNH215015 <duy362124.com>
2024-12-12 13:28:31 +0700

    api get cmtbypost

--- backend/src/routes/commentRoutes.js
+++ backend/src/routes/commentRoutes.js
@@ -1,11 +1,11 @@
 // routes/commentRoutes.js
 const express = require('express');
 const { createComment, getCommentsByPost } = require('../controllers/commentController');
-
+const upload = require('multer')(); 
 const router = express.Router();
 
 // Route để tạo bình luận
-router.post('/comments', createComment);
+router.post('/comments', upload.none(), createComment);
 
 // Route để lấy tất cả bình luận của một bài post
 router.get('/posts/:postId/comments', getCommentsByPost);
