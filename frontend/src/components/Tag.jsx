43079365a98e23890cc1fcc190fef39f9f4ee9eb
tungnx-0469 <nguyen.xuan.tung-d@sun-asterisk.com>
2024-12-28 12:07:01 +0700

    add click tabs events

--- frontend/src/components/Tag.jsx
+++ frontend/src/components/Tag.jsx
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types"
-
+import { useNavigate } from "react-router-dom"
 
 
 function Tag({ tag }) {
+  const navigate = useNavigate()
   return (
-    <div className="tag">
+    <div className="tag" onClick={() => navigate(`/tabs/${tag}`)} style={{cursor: "pointer"}}>
         {tag}
     </div>
   )
